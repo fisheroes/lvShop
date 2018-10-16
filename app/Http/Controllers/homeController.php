3dd15c8ae93d2c1b4722cd561ce9a53d0709378c
fisheroes <fisheroes@gmail.com>
2018-10-16 19:07:42 +0700

    first commit

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
+    public function index(){
+        return view("home");
+    }
     public function home(){
         return view("page-home");
     }
