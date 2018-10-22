a5f1105400915c25a65f9733ec983f5d4f40029d
fisheroes <fisheroes@gmail.com>
2018-10-22 15:12:55 +0700

    commit

--- resources/views/page-about.blade.php
+++ resources/views/page-about.blade.php
@@ -1,3 +1,31 @@
 @extends('template')
 @section('content')
+<style>
+    .table {
+        width: 50%; 
+        margin: 0 auto;
+        font-size: 24px;
+    }
+</style>
+
+<div class="container">
+    <table class="well table table-border">
+        <tr>
+            <td colspan="3"><img src="img/logo.jpg" id="img-block"></td>
+        </tr>
+        <tr>
+            <td class="text-center" width="1"><i class="fa fa-home"></i></td>
+            <td>Jl. dsad dsad dsa ds</td>
+        </tr>
+        <tr>
+            <td class="text-center" width="1"><i class="fa fa-at"></i></td>
+            <td>missenblacu@gmail.com</td>
+        </tr>
+        <tr>
+            <td class="text-center" width="1"><i class="fa fa-mobile-phone"></i></td>
+            <td>08132133213</td>
+        </tr>
+    </table>
+</div>
+
 @endsection
--- resources/views/page-order.blade.php
+++ resources/views/page-order.blade.php
@@ -1,3 +1,30 @@
 @extends('template')
 @section('content')
+
+<div class="container">
+    <h1 class="text-center">Cara Pemesanan</h1>
+    <table class="well table table-borderless" style="width: 50%; margin: 0 auto">
+        <tr>
+            <td width="1">1.</td>
+            <td>dsad dsad dsada dsadsa dsad dsad</td>
+        </tr>
+        <tr>
+            <td width="1">2.</td>
+            <td>dsad dsad dsada dsadsa dsad dsad</td>
+        </tr>
+        <tr>
+            <td width="1">3.</td>
+            <td>dsad dsad dsada dsadsa dsad dsad dsadsadsa dsad dsad dsad</td>
+        </tr>
+        <tr>
+            <td width="1">4.</td>
+            <td>dsad dsad dsada dsadsa dsad dsad dsad dsa dsa dsadsadsadsa dsa</td>
+        </tr>
+        <tr>
+            <td width="1">5.</td>
+            <td>dsad dsad dsada dsadsa dsad dsad</td>
+        </tr>
+    </table>
+</div>
+
 @endsection
